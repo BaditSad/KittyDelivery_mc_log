f77c4737252dcfd1c34e1cdf3a9a6e6ed4a849f4
BaditSad <bdumortier0@gmail.com>
2024-06-14 12:59:15 +0200

    docker init env

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
+require('dotenv').config(); // Charger les variables d'environnement à partir de .env
+
 const logRouter = require("./controllers/LogController");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 const app = express();
-const port = 3009;
+const port = process.env.PORT || 3009;
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/kittydelivery";
 
 app.use(cors());
-
-const db = require("./models");
-db.mongoose
-  .connect(db.url)
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch((err) => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
-
-//Ici on envoit les infos vers le front
-
 app.use(bodyParser.json());
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("Connected to MongoDB!");
+})
+.catch((err) => {
+  console.error("Error connecting to MongoDB:", err);
+  process.exit();
+});
 
 app.use("/", logRouter);
 
-app.listen(port, () => console.log(`app running on http://localhost:${port}`));
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
